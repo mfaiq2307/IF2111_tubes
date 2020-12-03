fe7532b02792a06cd91d904bced0fbc2d3539227
erwinfattah <68496276+erwinfattah@users.noreply.github.com>
2020-12-03 21:31:22 +0700

    Add files via upload

---
+++ circular_queue.c
@@ -0,0 +1,87 @@
+#include<stdlib.h>
+#include "circular_queue.h"
+
+/* ********* Prototype ********* */
+boolean IsEmpty (Queue Q) {
+    return Head(Q)== NIL && Tail(Q)== NIL;
+}
+boolean IsFull (Queue Q) {
+    return Length(Q) == MaxOrder(Q);
+}
+int Length (Queue Q) {
+    if (IsEmpty(Q))
+        return 0;
+    else if (Head(Q)>= Tail(Q))
+        return MaxOrder(Q)+ Tail(Q) - Head(Q);
+    else
+        return Tail(Q) - Head(Q);
+}
+int MaxLength (Queue Q) {
+    return MaxOrder(Q);
+}
+
+/* *** Kreator *** */
+Queue CreateQueue (int Max) {
+    Queue Q;
+    Q.Info = (ElType *) malloc (Max * sizeof(ElType));
+    Head(Q) = NIL;
+    Tail(Q)= NIL;
+    MaxOrder(Q) = Max;
+    return Q;
+}
+
+/* *** Destruktor *** */
+void DeleteQueue (Queue * Q) {
+    MaxOrder(*Q)= 0;
+    free(Q->Info);
+}
+
+/* *** Primitif Add/Delete *** */
+void Push (Queue * Q, int num, Item component, int price) {
+    /*Kamus Lokal*/
+    ElType X;
+    /*Algoritma*/
+    NoPelanggan(X)=num;
+    ListKomponen(X)=component;
+    Harga(X)=price;
+    
+    if (IsEmpty(*Q)) {
+        Head(*Q) = 0;
+        Tail(*Q) = 0;
+    }
+    InfoTail(*Q) = X;
+    Tail(*Q)++;
+    if (Tail(*Q) == MaxOrder(*Q)){
+        Tail(*Q) = 0;
+    }
+    
+    
+}
+ElType Pop (Queue * Q) {
+    ElType ret = InfoHead(*Q);
+    Head(*Q)++;
+    if (Head(*Q) == MaxOrder(*Q)) {
+        Head(*Q)= 0;
+    }
+    if (Head(*Q)==Tail(*Q)) {
+        Head(*Q)= NIL;
+        Tail(*Q) = NIL;
+    }
+    return ret;
+}
+ElType Front (Queue Q) {
+    return InfoHead(Q);
+}
+
+/* *** Utilitas *** */
+Queue CopyQueue (Queue Q) {
+    Queue baru = CreateQueue(MaxOrder(Q));
+    if (!IsEmpty(Q)) {
+        int tail = Head(Q);
+        while (Length(baru) != Length(Q)) {
+            Push(&baru, NoPelanggan(InfoTail(Q)),ListKomponen(InfoTail(Q)),Harga(InfoTail(Q)));
+            tail = (tail + 1) % MaxOrder(Q);
+        }
+    }
+    return baru;
+}
---
+++ order.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "circular_queue.h"
+
+void order(Queue *Q) {
+    int num = (rand() % 7);
+    int price;
+    int i;
+    int *kompo;
+    kompo=(int *) malloc(8*(sizeof(int)));
+    for(i=0;i<8;i++){
+        kompo[i]= (rand() % 3);
+    }
+
+    
+
+
+
+}
