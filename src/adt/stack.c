1b29588ff7605a7adc74dee6071b398474dbdbf5
fauzanrafif <68468383+fauzanrafif@users.noreply.github.com>
2020-12-05 03:16:54 +0700

    update

--- src/adt/stack.c
+++ src/adt/stack.c
@@ -15,7 +15,7 @@ void CreateEmpty (Stack *S)
 boolean IsEmpty (Stack S)
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
 {
-	return (Top(S) == Nil);
+	return Top(S) == Nil;
 }
 boolean IsFull (Stack S)
 /* Mengirim true jika tabel penampung nilai elemen stack penuh */
@@ -23,7 +23,7 @@ boolean IsFull (Stack S)
 	return Top(S) == MaxEl - 1;
 }
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Push (Stack * S, infotype X)
+void Push (Stack *S, infotype X)
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
@@ -32,7 +32,7 @@ void Push (Stack * S, infotype X)
 	InfoTop(*S) = X;
 }
 /* ************ Menghapus sebuah elemen Stack ************ */
-void Pop (Stack * S, infotype* X)
+void Pop (Stack *S, infotype *X)
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
