ff71f52ad764077dc16cceb34317bfc1bdd4e53c
faiq <mfaiq2307@gmail.com>
2020-12-05 05:02:11 +0700

    yu bisa yu

--- src/adt/inventory.c
+++ src/adt/inventory.c
@@ -1,51 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "parts.h"
+#include "inventory.h"
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor : create tabel kosong  */
-void MakePartEmpty(TabPart *T, int MaxElPart)
+void MakeInventoryEmpty(TabInventory *T, int MaxElInventory)
 {
-    /* I.S. T sembarang, MaxElPart > 0 */
-    /* F.S. Terbentuk tabel T kosong dengan kapasitas MaxElPart + 1 */
-    TP(*T) = (ElPartType *)malloc((MaxElPart + 1) * sizeof(ElPartType));
-    NeffPart(*T) = 0;
-    MaxElPart(*T) = MaxElPart;
+    /* I.S. T sembarang, MaxElInventory > 0 */
+    /* F.S. Terbentuk tabel T kosong dengan kapasitas MaxElInventory + 1 */
+    TInv(*T) = (ElInventoryType *)malloc((MaxElInventory + 1) * sizeof(ElInventoryType));
+    NeffInventory(*T) = 0;
+    MaxElInventory(*T) = MaxElInventory;
 }
 
-void DealokasiPart(TabPart *T)
+void DealokasiInventory(TabInventory *T)
 {
     /* I.S. T terdefinisi; */
-    /* F.S. TP(T) dikembalikan ke system, MaxElPart(T)=0; NeffPart(T)=0 */
-    free(TP(*T));
-    NeffPart(*T) = 0;
-    MaxElPart(*T) = 0;
+    /* F.S. TInv(T) dikembalikan ke system, MaxElInventory(T)=0; NeffInventory(T)=0 */
+    free(TInv(*T));
+    NeffInventory(*T) = 0;
+    MaxElInventory(*T) = 0;
 }
 
-IdxPartType GetFirstIdx(TabPart T)
+IdxInventoryType GetFirstIdx(TabInventory T)
 {
     /* Prekondisi : Tabel T tidak kosong */
     /* Mengirimkan indeks elemen T pertama */
     return IdxMin;
 }
-IdxPartType GetLastIdx(TabPart T)
+IdxInventoryType GetLastIdx(TabInventory T)
 {
     /* Prekondisi : Tabel T tidak kosong */
     /* Mengirimkan indeks elemen T terakhir */
-    return NeffPart(T) - GetFirstIdx(T) + 1;
+    return NeffInventory(T) - GetFirstIdx(T) + 1;
 }
 
 
 
 
 /* ********** Test Indeks yang valid ********** */
-boolean IsIdxValidPart(TabPart T, IdxPartType i)
+boolean IsIdxValidInventory(TabInventory T, IdxInventoryType i)
 {
     /* Mengirimkan true jika i adalah indeks yang valid utk ukuran tabel */
     /* yaitu antara indeks yang terdefinisi utk container*/
-    return ((i >= IdxMin) && (i <= MaxElPart(T)));
+    return ((i >= IdxMin) && (i <= MaxElInventory(T)));
 }
-boolean IsIdxEffPart(TabPart T, IdxPartType i)
+boolean IsIdxEffInventory(TabInventory T, IdxInventoryType i)
 {
     /* Mengirimkan true jika i adalah indeks yang terdefinisi utk tabel */
     /* yaitu antara FirstIdx(T)..LastIdx(T) */
@@ -54,20 +54,20 @@ boolean IsIdxEffPart(TabPart T, IdxPartType i)
 
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test tabel kosong *** */
-boolean IsEmptyPart(TabPart T)
+boolean IsEmptyInventory(TabInventory T)
 {
     /* Mengirimkan true jika tabel T kosong, mengirimkan false jika tidak */
-    return (NeffPart(T) == 0);
+    return (NeffInventory(T) == 0);
 }
-boolean IsFullPart(TabPart T)
+boolean IsFullInventory(TabInventory T)
 {
     /* Mengirimkan true jika tabel T penuh, mengirimkan false jika tidak */
-    return (NeffPart(T) == MaxElPart(T) - IdxMin + 1);
+    return (NeffInventory(T) == MaxElInventory(T) - IdxMin + 1);
 }
 
-/* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
+/* ********** BACA dan TULIS dengan INPUT/OUTInvUT device ********** */
 /* *** Mendefinisikan isi tabel dari pembacaan *** */
-void TulisIsiTabPart(TabPart T)
+void TulisIsiTabInventory(TabInventory T)
 {
     /* Proses : Menuliskan isi tabel dengan traversal, tabel ditulis di antara kurung siku;
    antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan karakter di depan,
@@ -76,35 +76,35 @@ void TulisIsiTabPart(TabPart T)
     /* F.S. Jika T tidak kosong: [e1,e2,...,en] */
     /* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
     /* Jika tabel kosong : menulis [] */
-    if (IsEmptyPart(T))
+    if (IsEmptyInventory(T))
     {
         printf("[]");
     }
     else
     {
         printf("[");
-        for (IdxPartType i = GetFirstIdx(T); i < GetLastIdx(T); i++)
+        for (IdxInventoryType i = GetFirstIdx(T); i < GetLastIdx(T); i++)
         {
-            printf("%s,", NamaPart(T, i).TabKata);
-            printf("%s,", HargaPart(T, i).TabKata);
-            printf("%s,", JenisPart(T, i).TabKata);
+            printf("%s,", NamaInventory(T, i).TabKata);
+            printf("%s,", BanyakInventory(T, i).TabKata);
+            printf("%s,", JenisInventory(T, i).TabKata);
             
         }
-        printf("%s]", NamaPart(T, GetLastIdx(T)).TabKata);
+        printf("%s]", NamaInventory(T, GetLastIdx(T)).TabKata);
     }
 }
 /* ********** SEARCHING ********** */
 /* ***  Perhatian : Tabel boleh kosong!! *** */
-IdxPartType Search1Part(TabPart T, Kata NamaParts)
+IdxInventoryType Search1Inventory(TabInventory T, Kata NamaInventorys)
 {
     /* Search apakah ada elemen tabel T yang bernilai X */
     /* Jika ada, menghasilkan indeks i terkecil, dengan elemen ke-i = X */
     /* Jika tidak ada, mengirimkan IdxUndef */
     /* Menghasilkan indeks tak terdefinisi (IdxUndef) jika tabel T kosong */
     /* Skema Searching yang digunakan bebas */
-    for (IdxPartType i = IdxMin; i <= NeffPart(T)-IdxMin+1; i++)
+    for (IdxInventoryType i = IdxMin; i <= NeffInventory(T)-IdxMin+1; i++)
     {
-        if ((IsStringSama(NamaPart(T,i).TabKata, NamaParts.TabKata)))
+        if ((IsStringSama(NamaInventory(T,i).TabKata, NamaInventorys.TabKata)))
         {
             return i;
         }
@@ -112,17 +112,17 @@ IdxPartType Search1Part(TabPart T, Kata NamaParts)
     //
     return IdxUndef;
 }
-boolean SearchBPart(TabPart T, Kata NamaParts)
+boolean SearchBInventory(TabInventory T, Kata NamaInventorys)
 {
     /* Search apakah ada elemen tabel T yang bernilai X */
     /* Jika ada, menghasilkan true, jika tidak ada menghasilkan false */
     /* Skema searching yang digunakan bebas */
     boolean Found = false;
-    IdxPartType i = IdxMin;
+    IdxInventoryType i = IdxMin;
     //
-    while ((!Found) && (i <= NeffPart(T)-IdxMin+1))
+    while ((!Found) && (i <= NeffInventory(T)-IdxMin+1))
     {
-        if ((IsStringSama(NamaPart(T,i).TabKata, NamaParts.TabKata)))
+        if ((IsStringSama(NamaInventory(T,i).TabKata, NamaInventorys.TabKata)))
         {
             Found = true;
         }
@@ -136,29 +136,29 @@ boolean SearchBPart(TabPart T, Kata NamaParts)
 }
 
 /* ********** OPERASI LAIN ********** */
-void CopyTabPart(TabPart Tin, TabPart *Tout)
+void CopyTabInventory(TabInventory Tin, TabInventory *Tout)
 {
     /* I.S. Tin terdefinisi tidak kosong, Tout sembarang */
-    /* F.S. Tout berisi salinan dari Tin (identik, NeffPart dan MaxElPart sama) */
+    /* F.S. Tout berisi salinan dari Tin (identik, NeffInventory dan MaxElInventory sama) */
     /* Proses : Menyalin isi Tin ke Tout */
-    TabPart ToutT;
-    MakePartEmpty(&ToutT, MaxElPart(Tin));
-    NeffPart(ToutT) = NeffPart(Tin);
+    TabInventory ToutT;
+    MakeInventoryEmpty(&ToutT, MaxElInventory(Tin));
+    NeffInventory(ToutT) = NeffInventory(Tin);
     //
-    for (IdxPartType i = GetFirstIdx(Tin); i <= GetLastIdx(Tin); i++)
+    for (IdxInventoryType i = GetFirstIdx(Tin); i <= GetLastIdx(Tin); i++)
     {
-        ElmtPart(ToutT, i) = ElmtPart(Tin, i);
+        ElmtInventory(ToutT, i) = ElmtInventory(Tin, i);
     }
     *Tout = ToutT;
 }
 
 /* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
 /* *** Menambahkan elemen terakhir *** */
-void AddAsLastElPart(TabPart *T, ElPartType X)
+void AddAsLastElInventory(TabInventory *T, ElInventoryType X)
 {
     /* Proses: Menambahkan X sebagai elemen terakhir tabel */
     /* I.S. Tabel T boleh kosong, tetapi tidak penuh */
     /* F.S. X adalah elemen terakhir T yang baru */
-    NeffPart(*T)++;
-    ElmtPart(*T, GetLastIdx(*T)) = X;
+    NeffInventory(*T)++;
+    ElmtInventory(*T, GetLastIdx(*T)) = X;
 }
--- src/adt/inventory.h
+++ src/adt/inventory.h
@@ -4,8 +4,8 @@
 /* Versi II : dengan banyaknya elemen didefinisikan secara eksplisit,
    memori tabel dinamik */
 
-#ifndef PARTS_H
-#define PARTS_H
+#ifndef INVENTORY_H
+#define INVENTORY_H
 
 #include "boolean.h"
 #include "mesinkata.h"
@@ -17,77 +17,77 @@
 /* Indeks tak terdefinisi*/
 
 /* Definisi elemen dan koleksi objek */
-typedef int IdxPartType; /* type indeks */
+typedef int IdxInventoryType; /* type indeks */
 typedef struct{
-    Kata NamaPart;
-    Kata HargaPart;
-    Kata JenisPart;
-} ElPartType;  /* type elemen tabel */
+    Kata NamaInventory;
+    Kata BanyakInventory;
+    Kata JenisInventory;
+} ElInventoryType;  /* type elemen tabel */
 typedef struct
 {
-    ElPartType *TP; /* memori tempat penyimpan elemen (container) */
-    int NeffPart;   /* >=0, banyaknya elemen efektif */
-    int MaxElPart;  /* ukuran elemen */
-} TabPart;
+    ElInventoryType *TInv; /* memori tempat penyimpan elemen (container) */
+    int NeffInventory;   /* >=0, banyaknya elemen efektif */
+    int MaxElInventory;  /* ukuran elemen */
+} TabInventory;
 /* Indeks yang digunakan [IdxMin..MaxEl] */
-/* Jika T adalah TabPart, cara deklarasi dan akses: */
-/* Deklarasi : T : TabPart */
+/* Jika T adalah TabInventory, cara deklarasi dan akses: */
+/* Deklarasi : T : TabInventory */
 /* Maka cara akses:
-   T.NeffPart  untuk mengetahui banyaknya elemen
+   T.NeffInventory  untuk mengetahui banyaknya elemen
    T.TI    untuk mengakses seluruh nilai elemen tabel
    T.TI[i] untuk mengakses elemen ke-i */
 /* Definisi :
-  Tabel kosong: T.NeffPart = 0
+  Tabel kosong: T.NeffInventory = 0
   Definisi elemen pertama : T.TI[i] dengan i=1
-  Definisi elemen terakhir yang terdefinisi: T.TI[i] dengan i=T.NeffPart */
+  Definisi elemen terakhir yang terdefinisi: T.TI[i] dengan i=T.NeffInventory */
 
 /* ********** SELEKTOR ********** */
-#define NeffPart(T) (T).NeffPart
-#define TP(T) (T).TP
-#define ElmtPart(T, i) (T).TP[(i)]
-#define MaxElPart(T) (T).MaxElPart
-#define NamaPart(T, i) (T).TP[(i)].NamaPart
-#define HargaPart(T, i) (T).TP[(i)].HargaPart
-#define JenisPart(T, i) (T).TP[(i)].JenisPart
+#define NeffInventory(T) (T).NeffInventory
+#define TInv(T) (T).TInv
+#define ElmtInventory(T, i) (T).TInv[(i)]
+#define MaxElInventory(T) (T).MaxElInventory
+#define NamaInventory(T, i) (T).TInv[(i)].NamaInventory
+#define BanyakInventory(T, i) (T).TInv[(i)].BanyakInventory
+#define JenisInventory(T, i) (T).TInv[(i)].JenisInventory
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor : create tabel kosong  */
-void MakePartEmpty(TabPart *T, int maxel);
+void MakeInventoryEmpty(TabInventory *T, int maxel);
 /* I.S. T sembarang, maxel > 0 */
 /* F.S. Terbentuk tabel T kosong dengan kapasitas maxel + 1 */
 
-void DealokasiPart(TabPart *T);
+void DealokasiInventory(TabInventory *T);
 /* I.S. T terdefinisi; */
-/* F.S. TI(T) dikembalikan ke system, MaxEl(T)=0; NeffPart(T)=0 */
+/* F.S. TI(T) dikembalikan ke system, MaxEl(T)=0; NeffInventory(T)=0 */
 
-IdxPartType GetFirstIdx(TabPart T);
+IdxInventoryType GetFirstIdx(TabInventory T);
 /* Prekondisi : Tabel T tidak kosong */
 /* Mengirimkan indeks elemen T pertama */
-IdxPartType GetLastIdx(TabPart T);
+IdxInventoryType GetLastIdx(TabInventory T);
 /* Prekondisi : Tabel T tidak kosong */
 /* Mengirimkan indeks elemen T terakhir */
 
 /* ********** Test Indeks yang valid ********** */
-boolean IsIdxValidPart(TabPart T, IdxPartType i);
+boolean IsIdxValidInventory(TabInventory T, IdxInventoryType i);
 
     /* Mengirimkan banyaknya elemen efektif tabel */
     /* Mengirimkan nol jika tabel kosong */
     /* *** Daya tampung container *** */
-boolean IsIdxEffPart(TabPart T, IdxPartType i);
+boolean IsIdxEffInventory(TabInventory T, IdxInventoryType i);
 /* Mengirimkan true jika i adalah indeks yang terdefinisi utk tabel */
 /* yaitu antara FirstIdx(T)..LastIdx(T) */
 
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test tabel kosong *** */
-boolean IsEmptyPart(TabPart T);
+boolean IsEmptyInventory(TabInventory T);
 /* Mengirimkan true jika tabel T kosong, mengirimkan false jika tidak */
 /* *** Test tabel penuh *** */
-boolean IsFullPart(TabPart T);
+boolean IsFullInventory(TabInventory T);
 /* Mengirimkan true jika tabel T penuh, mengirimkan false jika tidak */
 
-/* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
+/* ********** BACA dan TULIS dengan INPUT/OUTInvUT device ********** */
 /* *** Mendefinisikan isi tabel dari pembacaan *** */
-void TulisIsiTabPart(TabPart T);
+void TulisIsiTabInventory(TabInventory T);
 /* Proses : Menuliskan isi tabel dengan traversal, tabel ditulis di antara kurung siku;
    antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan karakter di depan,
    di tengah, atau di belakang, termasuk spasi dan enter */
@@ -98,26 +98,26 @@ void TulisIsiTabPart(TabPart T);
 
 /* ********** SEARCHING ********** */
 /* ***  Perhatian : Tabel boleh kosong!! *** */
-IdxPartType Search1Part(TabPart T, Kata NamaParts);
+IdxInventoryType Search1Inventory(TabInventory T, Kata NamaInventory);
 /* Search apakah ada elemen tabel T yang bernilai X */
 /* Jika ada, menghasilkan indeks i terkecil, dengan elemen ke-i = X */
 /* Jika tidak ada, mengirimkan IdxUndef */
 /* Menghasilkan indeks tak terdefinisi (IdxUndef) jika tabel T kosong */
 /* Skema Searching yang digunakan bebas */
-boolean SearchBPart(TabPart T, Kata NamaParts);
+boolean SearchBInventory(TabInventory T, Kata NamaInventory);
 /* Search apakah ada elemen tabel T yang bernilai X */
 /* Jika ada, menghasilkan true, jika tidak ada menghasilkan false */
 /* Skema searching yang digunakan bebas */
 
 /* ********** OPERASI LAIN ********** */
-void CopyTabPart(TabPart Tin, TabPart *Tout);
+void CopyTabInventory(TabInventory Tin, TabInventory *Tout);
 /* I.S. Tin terdefinisi tidak kosong, Tout sembarang */
-/* F.S. Tout berisi salinan dari Tin (identik, NeffPart dan MaxEl sama) */
+/* F.S. Tout berisi salinan dari Tin (identik, NeffInventory dan MaxEl sama) */
 /* Proses : Menyalin isi Tin ke Tout */
 
 /* ********** MENAMBAH DAN MENGHAPUS ELEMEN DI AKHIR ********** */
 /* *** Menambahkan elemen terakhir *** */
-void AddAsLastElPart(TabPart *T, ElPartType X);
+void AddAsLastElInventory(TabInventory *T, ElInventoryType X);
 /* Proses: Menambahkan X sebagai elemen terakhir tabel */
 /* I.S. Tabel T boleh kosong, tetapi tidak penuh */
 /* F.S. X adalah elemen terakhir T yang baru */
