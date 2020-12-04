82658187f63165b6f979890d44ba4e160c0d0fa5
peak-source <pandupuncak@gmail.com>
2020-12-04 17:03:47 +0700

    Create graph.h

---
+++ src/adt/graph.h
@@ -0,0 +1,33 @@
+#ifndef graph_H
+#define graph_H
+
+#include "boolean.h"
+
+
+#define Nil NULL
+
+typedef int infotype;
+typedef struct tElmtNode *addrNode;
+typedef struct tElmtConNode *addrCon;
+typedef struct tElmtNode {
+    infotype Info;
+    /*int NPred;*/
+    addrCon Hub;
+    addrNode Next;
+} ElmtNode;
+
+typedef struct tElmtConNode{
+    addrNode Connect;
+    addrCon NextCon;
+} ConnectNode;
+
+typedef struct { 
+    addrNode First;
+} Graph;
+
+#define Info(G) (G)->Info
+#define Hub(G) (G)->Hub
+#define Next(G) (G)->Next
+#define Connect(G) (G)->Connect
+#define NextC(G) (G)->NextCon
+
