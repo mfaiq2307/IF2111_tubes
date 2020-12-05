624367f1e3e073ee305c174a476a0fd23dcca4e7
peak-source <pandupuncak@gmail.com>
2020-12-05 07:36:40 +0700

    Purify Command habis Loop di Main

---
+++ src/adt/driver.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "mesinkata.h"
+
+
+int main() {
+    char filename[] = "parts.txt";
+    STARTKATA_File(filename);
+    while (!EndKata) { 
+        /*for (int i=0;i<CKata.Length;i++) {
+            printf("%s",CKata.TabKata);
+        */
+        printf("%s",CKata.TabKata); /*printf("\n");*/ 
+        ADVKATA_File();
+        printf("\n");
+    } 
+    return 0; 
+} 
